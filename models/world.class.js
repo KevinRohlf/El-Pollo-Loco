79d35b68294cddfc44b8581561b2f701bbb1592a
Kevin Rohlf <123676073+KevinRohlf@users.noreply.github.com>
2023-02-27 23:52:29 +0100

    Added & edit some Things
    - add width and height to movable-object.class.js
    
    - add draw to world.class.js

--- models/world.class.js
+++ models/world.class.js
@@ -5,11 +5,18 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    ctx;
 
-
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
     draw() {
-
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        for (let i = 0; i < this.enemies.length; i++) {
+            this.ctx.drawImage(this.enemies[i].img, this.enemies[i].x, this.enemies[i].y, this.enemies[i].width, this.enemies[i].height)
+        }
 
     }
 }
